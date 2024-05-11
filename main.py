266cef1a0fe5774cc823d7cd4c168c9b05985a7b
adrianix1 <adrian.panczyk@wp.pl>
2024-05-11 14:56:45 +0200

    First todo - mechanism of mark chosing - complete.

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 # TODO 1: input which mark player wants to chose
-# TODO 2: print positions possible, 9 digits to chose
+
+def convert_0(mark):
+    if mark == "0":
+        mark = "O"
+    return mark
+
+
+player_mark = -1
+while player_mark not in ("X", "O"):
+    player_mark = input("Provide a valid mark (X, O): ").upper()
+    player_mark = convert_0(player_mark)
+
+# TODO 2: print positions possible, 9 digits to chose from
+
+
+
 # TODO 3: input which position player wants to put his mark
 # TODO 4: after player puts his mark computer also does it and board is plotted again
-# TODO 5: after the end of game input if player wants to play again
+# TODO 5: check if computer or player wins
+# TODO 6: after the end of game input if player wants to play again
