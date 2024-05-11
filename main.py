1c67c19a2f5d155d2a6ca1e96f398afb6e3d66f5
adrianix1 <adrian.panczyk@wp.pl>
2024-05-11 14:21:33 +0200

    Initial commit, list TODOS.

---
+++ main.py
@@ -0,0 +1,5 @@
+# TODO 1: input which mark player wants to chose
+# TODO 2: print positions possible, 9 digits to chose
+# TODO 3: input which position player wants to put his mark
+# TODO 4: after player puts his mark computer also does it and board is plotted again
+# TODO 5: after the end of game input if player wants to play again
