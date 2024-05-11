ed7e8f8e0d8416689182104d78b92d1d5d6b5e47
adrianix1 <adrian.panczyk@wp.pl>
2024-05-11 15:29:46 +0200

    Second todo - initial plot of a board - complete. Modify TODOS 3 & 5

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+from random import randint
+START_BOARD = "1|2|3\n-+-+-\n4|5|6\n-+-+-\n7|8|9"
+
 # TODO 1: input which mark player wants to chose
+
 
 def convert_0(mark):
     if mark == "0":
@@ -12,10 +16,17 @@ while player_mark not in ("X", "O"):
     player_mark = convert_0(player_mark)
 
 # TODO 2: print positions possible, 9 digits to chose from
+if player_mark == "X":
+    computer_mark = "O"
+    print(START_BOARD)
+else:
+    computer_mark = "X"
+    computer_move = str(randint(1, 9))
+    board = START_BOARD
+    board = board.replace(computer_move, computer_mark)
+    print(board)
 
-
-
-# TODO 3: input which position player wants to put his mark
+# TODO 3: input which position player wants to put his mark, check taken positions, keep track of moves in list
 # TODO 4: after player puts his mark computer also does it and board is plotted again
-# TODO 5: check if computer or player wins
+# TODO 5: check if computer or player wins, compare with winning combinations
 # TODO 6: after the end of game input if player wants to play again
